e3ba52402320d3e47a7fd8cd198c2ec12570b923
amozalev <mozalev.alexey@gmail.com>
2023-08-19 18:45:07 +0300

    Updated AdvantagesSection.

--- src/components/Sections/AdvantagesSection/AdvantagesSection.Styled.ts
+++ src/components/Sections/AdvantagesSection/AdvantagesSection.Styled.ts
@@ -1,9 +1,27 @@
 import styled from 'styled-components';
 
 export const StyledAdvantagesSection = styled.div`
+    display: grid;
+    gap: 40px;
+    grid-template: 1fr / 1fr;
+    grid-template-areas: 'article';
+    padding: 50px 0;
+
+    .article-outer {
+        display: grid;
+        grid-area: article;
+        align-items: center;
+    }
+
+    .article {
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+
     .advantages {
         display: grid;
-        grid-template: repeat(3, 1fr) / 1fr auto;
+        grid-template: repeat(3, 1fr) / 50px auto;
         gap: 32px 16px;
     }
 
@@ -11,5 +29,47 @@ export const StyledAdvantagesSection = styled.div`
         display: flex;
         flex-direction: column;
         gap: 4px;
+    }
+
+    .gallery-outer {
+        display: none;
+    }
+
+    @media (min-width: 1180px) {
+        gap: 90px;
+        grid-template: 1fr / 1fr 1fr;
+        grid-template-areas: 'article gallery';
+        padding: 80px 0 100px;
+
+        .gallery-outer {
+            display: grid;
+            grid-area: gallery;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .gallery {
+            display: grid;
+            grid-template: repeat(2, 250px) / repeat(2, 250px);
+            gap: 10px;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .image {
+            width: 250px;
+            height: 250px;
+            object-fit: cover;
+        }
+
+        .image:nth-child(1),
+        .image:nth-child(4) {
+            border-radius: 0 50px;
+        }
+
+        .image:nth-child(2),
+        .image:nth-child(3) {
+            border-radius: 50px 0;
+        }
     }
 `;
--- src/components/Sections/AdvantagesSection/AdvantagesSection.tsx
+++ src/components/Sections/AdvantagesSection/AdvantagesSection.tsx
@@ -1,68 +1,87 @@
 import * as S from './AdvantagesSection.Styled';
 import * as T from '../../Text/TextStyles';
 import ColoredSection from '../../Section/ColoredSection';
-import Article from '../../Article/Article';
 import { TouristsIcon } from '../../Icons/TouristsIcon';
 import { ShieldIcon } from '../../Icons/ShieldIcon';
 import { LabelIcon } from '../../Icons/LabelIcon';
 import AdvantageIconCard from '../../AdvantageIconCard/AdvantageIconCard';
 import { Button } from '../../Button/Button';
+import SectionHeader from '../../SectionHeader/SectionHeader';
+import backpackerImg from '../../../assets/images/advantages_backpacker_lake.jpg';
+import fieldMountainImg from '../../../assets/images/advantages_field_mountain.jpg';
+import fireImg from '../../../assets/images/advantages_fire.jpg';
+import yellowCoatImg from '../../../assets/images/advantages_yellow_coat.jpg';
 
 const AdvantagesSection = () => {
     return (
-        <ColoredSection
-            description="наше предложение"
-            title="Лучшие программы для тебя"
-            backgroundColor="#F3F5F4"
-        >
+        <ColoredSection backgroundColor="#F3F5F4">
             <S.StyledAdvantagesSection>
-                <Article marginTop={32} gap={32}>
-                    <T.Text1>
-                        Его корни уходят в один фрагмент классической латыни 45
-                        года н.э., то есть более двух тысячелетий назад. Ричард
-                        МакКлинток, профессор латыни из колледжа.
-                    </T.Text1>
-                    <div className="advantages">
-                        <AdvantageIconCard color={'#E5EEEB'}>
-                            <TouristsIcon />
-                        </AdvantageIconCard>
-                        <div className="advantage">
-                            <T.Title4>Опытный гид</T.Title4>
-                            <T.Text2>
-                                Для современного мира базовый вектор развития
-                                предполагает независимые способы реализации
-                                соответствующих условий активизации.
-                            </T.Text2>
+                <div className="article-outer">
+                    <article className="article">
+                        <SectionHeader
+                            description="наше предложение"
+                            title="Лучшие программы для тебя"
+                        />
+                        <T.Text1>
+                            Его корни уходят в один фрагмент классической латыни
+                            45 года н.э., то есть более двух тысячелетий назад.
+                            Ричард МакКлинток, профессор латыни из колледжа.
+                        </T.Text1>
+                        <div className="advantages">
+                            <AdvantageIconCard color={'#E5EEEB'}>
+                                <TouristsIcon />
+                            </AdvantageIconCard>
+                            <div className="advantage">
+                                <T.Title4>Опытный гид</T.Title4>
+                                <T.Text2>
+                                    Для современного мира базовый вектор
+                                    развития предполагает независимые способы
+                                    реализации соответствующих условий
+                                    активизации.
+                                </T.Text2>
+                            </div>
+                            <AdvantageIconCard color={'#E3E6EE'}>
+                                <ShieldIcon />
+                            </AdvantageIconCard>
+                            <div className="advantage">
+                                <T.Title4>Безопасный поход</T.Title4>
+                                <T.Text2>
+                                    Для современного мира базовый вектор
+                                    развития предполагает независимые способы
+                                    реализации соответствующих условий
+                                    активизации.
+                                </T.Text2>
+                            </div>
+                            <AdvantageIconCard color={'#F3F1E1'}>
+                                <LabelIcon />
+                            </AdvantageIconCard>
+                            <div className="advantage">
+                                <T.Title4>Лояльные цены</T.Title4>
+                                <T.Text2>
+                                    Для современного мира базовый вектор
+                                    развития предполагает независимые способы
+                                    реализации соответствующих условий
+                                    активизации.
+                                </T.Text2>
+                            </div>
                         </div>
-                        <AdvantageIconCard color={'#E3E6EE'}>
-                            <ShieldIcon />
-                        </AdvantageIconCard>
-                        <div className="advantage">
-                            <T.Title4>Безопасный поход</T.Title4>
-                            <T.Text2>
-                                Для современного мира базовый вектор развития
-                                предполагает независимые способы реализации
-                                соответствующих условий активизации.
-                            </T.Text2>
+                        <div>
+                            <Button
+                                title="Стоимость программ"
+                                color="#FDFDFD"
+                                backgroundColor="#1A3E3E"
+                            />
                         </div>
-                        <AdvantageIconCard color={'#F3F1E1'}>
-                            <LabelIcon />
-                        </AdvantageIconCard>
-                        <div className="advantage">
-                            <T.Title4>Лояльные цены</T.Title4>
-                            <T.Text2>
-                                Для современного мира базовый вектор развития
-                                предполагает независимые способы реализации
-                                соответствующих условий активизации.
-                            </T.Text2>
-                        </div>
+                    </article>
+                </div>
+                <div className="gallery-outer">
+                    <div className="gallery">
+                        <img className="image" src={backpackerImg} />
+                        <img className="image" src={fieldMountainImg} />
+                        <img className="image" src={fireImg} />
+                        <img className="image" src={yellowCoatImg} />
                     </div>
-                    <Button
-                        title="Стоимость программ"
-                        color="#FDFDFD"
-                        backgroundColor="#1A3E3E"
-                    />
-                </Article>
+                </div>
             </S.StyledAdvantagesSection>
         </ColoredSection>
     );
